747704d2540fc4a016a82b1c009aa16ca519d39a
etridenour <etridenour@gmail.com>
2018-12-03 20:38:00 -0600

    removed build from gitignore

---
+++ client/build/precache-manifest.ce36d9a0290ea819769969f94cfca8af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/static/media/loading.f4404720.gif"
+  },
+  {
+    "revision": "859da1cd99af82cde4f61462e8ae9b0f",
+    "url": "/static/media/bg.859da1cd.jpg"
+  },
+  {
+    "revision": "2526378d14011b87e2106de9b013c39b",
+    "url": "/static/media/aurora.2526378d.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c777fc33ee4b6a897aa1",
+    "url": "/static/js/main.c777fc33.chunk.js"
+  },
+  {
+    "revision": "21618cf24dc612df3082",
+    "url": "/static/js/1.21618cf2.chunk.js"
+  },
+  {
+    "revision": "c777fc33ee4b6a897aa1",
+    "url": "/static/css/main.67126339.chunk.css"
+  },
+  {
+    "revision": "21618cf24dc612df3082",
+    "url": "/static/css/1.2581dbd9.chunk.css"
+  },
+  {
+    "revision": "567e0f6c5ff5ffed4b6df3e6332d42f6",
+    "url": "/index.html"
+  }
+];
